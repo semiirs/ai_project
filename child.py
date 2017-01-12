82795c66d60223befa4bbaa7b725dda9b757872c
semaars <existencialists@inbox.lv>
2017-01-12 20:23:04 +0200

    Renamed production db.

--- child.py
+++ child.py
@@ -5,19 +5,20 @@ import sys
 import common.utils
 import sqlite3
 
+# conn = sqlite3.connect('prod_events.db')
 conn = sqlite3.connect('events.db')
 c = conn.cursor()
 
 
 def add_experience(event):
-    purchases = [(str(datetime.datetime.now()), str(event))
-                ]
-    c.executemany('INSERT INTO experiences (date, event) VALUES (?,?)', purchases)
+    event = [(str(datetime.datetime.now()), str(event))]
+    c.executemany('INSERT INTO experiences (date, event) VALUES (?,?)', event)
     conn.commit()
 
 
 def learn():
-    pass
+    for row in c.execute('SELECT * FROM experiences ORDER BY date'):
+        print(row)
 
 
 def watch_world():
@@ -35,6 +36,8 @@ def watch_world():
             os.remove(common.utils.event_source_file)
 
 watch_world()
+learn()
+
 conn.close()
 time.sleep(0.5)
 print("Re-running self...")
